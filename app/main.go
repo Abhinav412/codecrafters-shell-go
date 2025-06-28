b0431934c9fa45f604620e517fa0cee15e03e46d
Abhinav <abhinavdasu0412@gmail.com>
2025-06-28 20:24:26 +0530

    The type builtin: builtins

--- app/main.go
+++ app/main.go
@@ -7,39 +7,58 @@ import (
 	"strings"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
+var COMMANDS map[string]func([]string)
+
+func init() {
+	COMMANDS = map[string]func([]string){
+		"echo": echoCMD,
+		"type": typeCMD,
+		"exit": exitCMD,
+	}
+}
+
+func exitCMD(input []string) {
+	if len(input) == 1 && input[0] == "0" {
+		os.Exit(0)
+	}
+	os.Exit(1)
+}
+
+func echoCMD(input []string) {
+	fmt.Println(strings.Join(input, " "))
+}
+
+func typeCMD(input []string) {
+	command := strings.Join(input, " ")
+	_, ok := COMMANDS[command]
+	if !ok {
+		fmt.Println("%v: not found\n", command)
+		return
+	}
+	fmt.Println("%v is a shell builtin\n", command)
+}
+
+func evaluate(input string) {
+	input = strings.TrimSpace(input)
+	args := strings.Split(input, " ")
+	command, optional := args[0], args[1:]
+
+	output, ok := COMMANDS[command]
+	if !ok {
+		fmt.Println("%v: command not found\n", command)
+		return
+	}
+	output(optional)
+}
 
 func main() {
-	// Uncomment this block to pass the first stage
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-
-		// Wait for user input
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error reading input: ", err)
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
-		if command[:len(command)-1] == "exit 0" {
-			break
-		}
-		if strings.HasPrefix(command, "echo ") {
-			fmt.Println(command[5 : len(command)-1])
-		} else if strings.HasPrefix(command, "type") {
-			argument := command[5:]
-			switch argument {
-			case "echo":
-				fmt.Println("echo is a shell builtin")
-			case "exit":
-				fmt.Println("exit is a shell builtin")
-			case "type":
-				fmt.Println("type is a shell builtin")
-			default:
-				fmt.Println(argument + ": not found")
-			}
-		} else {
-			fmt.Println(command[:len(command)-1] + ": command not found")
-		}
+		evaluate(command)
 	}
 }
